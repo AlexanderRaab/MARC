ddae88f978fc1cdb7cb84ba4281ddcc6ac9866c4
AlexanderRaab <92386985+AlexanderRaab@users.noreply.github.com>
2024-03-18 19:23:49 +0100

    Added initial nav2 config

--- 03 Software/01 ROS2/marc/marc_bringup/launch/MARC.launch.py
+++ 03 Software/01 ROS2/marc/marc_bringup/launch/MARC.launch.py
@@ -20,12 +20,17 @@ from launch_ros.parameter_descriptions import ParameterValue
 pkg_share_bringup = get_package_share_directory('marc_bringup')
 pkg_share_description = get_package_share_directory('marc_description')
 pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+pkg_nav2 = get_package_share_directory('nav2_bringup')
+pkg_nav2_bt = get_package_share_directory('nav2_bt_navigator')
 
 default_device_config_path = os.path.join(pkg_share_bringup, 'config', 'device_config.yaml')
 default_robot_model_path = os.path.join(pkg_share_description, 'src', 'robot', 'MARC.urdf.xacro')
 default_world_file_path = os.path.join(pkg_share_bringup, 'src', 'worlds', 'mt_og5.world')
 default_rviz_config_path = os.path.join(pkg_share_bringup, 'config', 'default.rviz')
-
+default_nav2_config_path = os.path.join(pkg_share_bringup, 'config', 'nav2_config.yaml')
+default_map_path = os.path.join(pkg_share_bringup, 'src', 'maps', 'mt_og5.yaml')
+default_behavior_tree_xml_path = os.path.join(pkg_nav2_bt, 'behavior_trees', 'navigate_w_replanning_and_recovery.xml')
+default_nav2_config_path = os.path.join(pkg_share_bringup, 'config', 'nav2_config.yaml')
 
 # Setup parameter confinguration
 configurable_parameters = [
@@ -33,6 +38,9 @@ configurable_parameters = [
     {'name': 'robot_model',                   	'default': default_robot_model_path,         			'description': 'Robot model file'},
     {'name': 'world_file',                    	'default': default_world_file_path,         			'description': 'Gazebo world file'},
     {'name': 'rviz_config',                   	'default': default_rviz_config_path,         			'description': 'rviz config file'},
+    {'name': 'nav2_config',						'default': default_nav2_config_path,					'description': 'nav2 config file'},
+    {'name': 'map_yaml',						'default': default_map_path,							'description': 'Map file'},
+ 	{'name': 'nav2_bt_config',					'default': default_behavior_tree_xml_path,				'description': 'Behaviour tree config file for nav2'},
     {'name': 'namespace',						'default': "",											'description': 'Application namespace'},
 	
 	# Operation modes
@@ -46,6 +54,10 @@ configurable_parameters = [
     {'name': 'use_plattform',					'default': True,										'description': 'Activate the differential drive plattform'},
     {'name': 'use_lidar',						'default': True,										'description': 'Activate the main lidar scanner'},
     {'name': 'use_camera',						'default': True,										'description': 'Activate the main camera'},
+    
+	# Software components
+    {'name': 'use_nav2',						'default': True,										'description': 'Activate the configured nav2 stack'},
+    {'name': 'nav2_slam',						'default': False,										'description': 'Activate the nav2 SLAM'},
     ]
 
 def declare_configurable_parameters(parameters):
@@ -92,7 +104,20 @@ def generate_launch_description():
 		launch_arguments={
 				'use_sim_time': launch_config['use_sim_time'],
 				'namespace': launch_config['namespace']}.items())
-
+      
+	# Nav2
+	start_nav2 = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_nav2, 'launch', 'bringup_launch.py')),
+        condition=IfCondition(launch_config['use_nav2']),
+        launch_arguments = {
+            'namespace': launch_config['namespace'],
+			'use_namespace': "True",
+            'slam': launch_config['nav2_slam'],
+            'map': launch_config['map_yaml'],
+            'use_sim_time': launch_config['use_sim_time'],
+            'params_file': launch_config['nav2_config'],
+            'default_bt_xml_filename': launch_config['nav2_bt_config'],
+            'autostart': "True"}.items())
 
   	# Launch RViz
 	start_rviz = Node(
@@ -113,7 +138,8 @@ def generate_launch_description():
 	ld.add_action(start_core)
 	ld.add_action(start_simulation)
 	ld.add_action(start_physical)
-      
-	ld.add_action(TimerAction(period=1., actions=[start_rviz]))
+
+	ld.add_action(TimerAction(period=2., actions=[start_nav2]))  
+	ld.add_action(TimerAction(period=6., actions=[start_rviz]))
 
 	return ld
